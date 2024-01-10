c2f40cf3c9ac0fc6ebcfc750c576346c908c152a
awerks <sustila24@gmail.com>
2024-01-10 02:16:24 +0100

    Delete .history directory

--- .history/_static/mgslider_20240110000010.js
+++
@@ -1,6 +0,0 @@
-mgslider.prototype.hide = function () {
-    document.getElementById(this.id()).style.display = "block";
-    document.getElementById(this.id("show")).style.visibility = "visible";
-    document.getElementById(this.id("show")).style.textAlign = "left";
-    document.getElementById(this.id("before")).style.display = "none";
-};
